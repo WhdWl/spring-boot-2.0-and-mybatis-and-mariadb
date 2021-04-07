cba3acecdc6c88642d0d8e9b16c95cb51889f92c
son <enepr90@naver.com>
2021-04-07 18:00:56 +0900

    ArticleService, ArticleServiceImpl, articleDto 구현 및 세팅, getList() 함수를 구현(일단 가짜 데이터 리턴하도록)

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,13 +1,28 @@
 package com.sbs.starter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sbs.starter.dto.Article;
+import com.sbs.starter.service.ArticleService;
+
+import groovy.util.logging.Slf4j;
+import jdk.internal.org.jline.utils.Log;
+
 @Controller
+@Slf4j // 이걸 해야 log.info() 등 가능..
 public class ArticleController {
+	
+	@Autowired
+	private ArticleService articleService;
 	
 	@RequestMapping("/article/list")
 	public String showList() {
+		List<Article> list = articleService.getList();
+		Log.info("list : " + list);
 		return "article/list";
 	}
 	
---
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -0,0 +1,28 @@
+package com.sbs.starter.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sbs.starter.dto.Article;
+
+@Service("articleService")
+public class ArticleServiceImpl implements ArticleService {
+
+	@Override
+	public List<Article> getList() {
+		// 가짜 데이터 중간확인용
+		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용2");
+		Article article2 = new Article(2, "2019-08-20 12:12:13", "제목2", "내용2");
+		Article article3 = new Article(3, "2019-08-20 12:12:14", "제목3", "내용3");
+		
+		List<Article> list = new ArrayList<>(); // Java 7.0 이상버전에서 가능 제너릭 타입 안적어도 됩니다.
+		list.add(article1);
+		list.add(article2);
+		list.add(article3);
+		
+		return list;
+	}
+
+}
